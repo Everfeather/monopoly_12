ea1976e5c936f159e36b5be8e6bc7bb4d6600260
Everfeather <everfether@gmail.com>
2021-10-19 14:43:43 -0400

    Added Property class, created getters, and a buildingPurchased methode to change the appropriate object variables.

---
+++ src/Property.java
@@ -0,0 +1,59 @@
+public class Property extends GameBoardSquare {
+
+
+    private int colourSetSize;
+    private int cost;
+    private int buildingCost;
+    private int rent;
+    private Player owner;
+    private PropertyType propertyType;
+    private int numBuildings;
+
+    public Property(String name, int cost, int colourSetSize, PropertyType propertyType) {
+        super(squareType.PROPERTY,name);
+        this.colourSetSize = colourSetSize;
+        this.cost = cost;
+        this.propertyType = propertyType;
+        this.rent = (int) (cost * 0.1);
+        this.buildingCost = (int) (cost * 0.55);
+        this.owner = null;
+        this.numBuildings = 0;
+    }
+
+    /**
+     * This methode changes the relevant attributes of the property accordingly when a building is purchased.
+     * increases the number of buildings by 1.
+     * increases the rent.
+     */
+    public void buildingPurchased(){
+        this.numBuildings ++;
+        this.rent += (int) (this.numBuildings * this.cost * 0.4);
+    }
+    public int getColourSetSize() {
+        return colourSetSize;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getBuildingCost() {
+        return buildingCost;
+    }
+
+    public int getRent() {
+        return rent;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public PropertyType getPropertyType() {
+        return propertyType;
+    }
+
+    public int getNumBuildings() {
+        return numBuildings;
+    }
+}
