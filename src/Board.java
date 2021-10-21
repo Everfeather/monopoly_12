a284a89a0ec4261bb4dc1754f3dbbf00a6652b06
unknown <Wangshujian1999@gmail.com>
2021-10-21 17:14:29 -0400

    instantiate board with 40 squares

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 
 public class Board{
-	private static final int boardsize = 40;
+	private static final int BOARDSIZE = 40;
 	private ArrayList <GameBoardSquare> board;
+
 	
 	public Board{
-		this.board = new Arraylist<GameBoardSquare>();
+		this.board = new ArrayList<GameBoardSquare>(BOARDSIZE);
+		//this.board = new Arraylist<GameBoardSquare>();
+
 		
 	}
 	public addSquare(SquareType type, String name){
