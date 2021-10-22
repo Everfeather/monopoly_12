9f8b9570ee19c11c8328f9f41919a97ba313cded
Everfeather <everfether@gmail.com>
2021-10-22 13:29:02 -0400

    GameController: added: payRent(), turn(), MAXNUMPLAYER and counter
    GameBoardSquare:
    added: getName()
    
    Player:
    added: increaseBalance(), decreaseBalance()
    
    Property:
    added: toString()

--- src/GameController.java
+++ src/GameController.java
@@ -1,2 +1,34 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class GameController {
+    private static final int MAXNUMPLAYERS = 4;
+    private int counter = 0;
+    private Board board;
+    private Die[] dice;
+    private Player[] players;
+    private Player curPlayer;
+
+
+    public void Turn(){
+    counter++;
+        if(counter > MAXNUMPLAYERS){
+            counter -= players.length;
+        }
+        curPlayer = players[counter % players.length];
+    }
+
+    public void payRent(Player payee,Player payer,Property property){
+        int cost = property.getCost();
+
+        if(payer.getBalance() >= cost){
+            payer.decreaseBalance(cost);
+            payee.increaseBalance(cost);
+        }else{
+            payee.increaseBalance(payer.getBalance());
+            payer.decreaseBalance(cost);
+            System.out.println("You cannot afford rent, you have gone bankrupt!");
+            payer.setBankrupt(true);
+        }
+    }
 }
